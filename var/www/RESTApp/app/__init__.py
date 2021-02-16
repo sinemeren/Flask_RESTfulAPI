9f79406c62cb3a703ef8210df639d02f2fa418dd
sinemeren <sinem.eren.me@gmail.com>
2021-02-16 20:40:49 +0300

    new init file

---
+++ var/www/RESTApp/app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from flask_restful import Api
+
+FlaskAppInstance = Flask(__name__)
+
+from app.api.views import HelloWorld
+
+apiInstance= Api(FlaskAppInstance)
+apiInstance.add_resource(HelloWorld,'/helloworld')
