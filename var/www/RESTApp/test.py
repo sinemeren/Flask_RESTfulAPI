9acdf215b8f9f9d08b9df9e54578c2f1e3d82d45
sinemeren <sinem.eren.me@gmail.com>
2021-02-17 19:34:08 +0300

    test for the post and get requests for the video

--- var/www/RESTApp/test.py
+++ var/www/RESTApp/test.py
@@ -4,5 +4,8 @@ BASE = "http://127.0.0.1:5000/"
 
 
 
-response_post = requests.put(BASE + "video/1", {"likes": 10})
+response_post = requests.put(BASE + "video/1", {"likes": 10, "name": "Tim", "views": 100000})
 print(response_post.json())
+input()
+response_get = requests.get(BASE + "video/1")
+print(response_get.json())
