2f6257e7d45abf80ca39c184b1d39d89d7726147
sinemeren <sinem.eren.me@gmail.com>
2021-02-17 20:01:23 +0300

    test for delete request and test for sending multiple post request

--- var/www/RESTApp/test.py
+++ var/www/RESTApp/test.py
@@ -2,10 +2,16 @@ import requests
 from app import FlaskAppInstance
 BASE = "http://127.0.0.1:5000/"
 
+data = [{"likes": 78, "name": "Joe", "views": 100000}, 
+        {"likes": 10000, "name": "How to make REST API", "views": 80000},
+        {"likes": 35, "name": "Tim", "views": 2000}]
 
-
-response_post = requests.put(BASE + "video/1", {"likes": 10, "name": "Tim", "views": 100000})
-print(response_post.json())
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
 input()
-response_get = requests.get(BASE + "video/1")
-print(response_get.json())
+response = requests.delete(BASE + "video/0")
+print(response) 
+input()
+response = requests.get(BASE + "video/2")
+print(response.json())
