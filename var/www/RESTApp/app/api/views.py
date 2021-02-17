358890f5834a6c70a39ce68aa62bd860d4989da9
sinemeren <sinem.eren.me@gmail.com>
2021-02-17 20:01:51 +0300

    delete request and abort functions

--- var/www/RESTApp/app/api/views.py
+++ var/www/RESTApp/app/api/views.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import  Api, Resource, reqparse
+from flask_restful import  Api, Resource, reqparse, abort
 
 # new request parser object, automatically parse the request is being sent and make sure that it fits the
 # kind of guidelines and has the correct information in it 
@@ -11,15 +11,31 @@ video_put_args.add_argument("likes", type = int, help = "Likes on the video", re
 
 videos = {}
 
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message = "Could not find video...")
+
+def abort_if_video_id_exists(video_id):
+    if video_id in videos:
+        abort(404, message = "video already exist with that id...")
 
 # HelloWorld class is inherited from Resource
 class Video(Resource):
 
     def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_id_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
-        return videos[video_id]
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return ' ', 204
+
+
 
