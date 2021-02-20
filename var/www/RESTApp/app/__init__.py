f0113d076bfc3a893ce9e60937be09b40f93e990
sinemeren <sinem.eren.me@gmail.com>
2021-02-20 21:50:34 +0300

    remove the circular dependency error

--- var/www/RESTApp/app/__init__.py
+++ var/www/RESTApp/app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 FlaskAppInstance = Flask(__name__)
 
-from app.api.views import Video
+# database
+FlaskAppInstance.config.from_pyfile('config/development.cfg')
+db = SQLAlchemy(FlaskAppInstance)
+db.create_all()
+
+#from app.api.views import Video
 
 apiInstance= Api(FlaskAppInstance)
-apiInstance.add_resource(Video, '/video/<int:video_id>')
+
